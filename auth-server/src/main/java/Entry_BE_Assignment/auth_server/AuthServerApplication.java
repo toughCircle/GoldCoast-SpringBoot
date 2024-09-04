61d2a71373ddb9531a824ab48cdd4bf6af3e685c
toughCircle <toughCircle@gmail.com>
2024-09-04 11:13:02 +0900

    feat: 프로젝트 초기 디렉토리 구조 설정

---
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/AuthServerApplication.java
@@ -0,0 +1,13 @@
+package Entry_BE_Assignment.auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
---
+++ resource-server/src/test/java/Entry_BE_Assignment/resource_server/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Entry_BE_Assignment.resource_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
