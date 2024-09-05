2cfe8581be1922b8f311cc7c9a646e0a376002f1
toughCircle <toughCircle@gmail.com>
2024-09-05 21:28:29 +0900

    feat: BaseEntity 생성 (#2)

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/AuthServerApplication.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/AuthServerApplication.java
@@ -2,8 +2,10 @@ package Entry_BE_Assignment.auth_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
