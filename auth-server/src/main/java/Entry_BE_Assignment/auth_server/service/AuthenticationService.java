9b9d702c11c99afc11f0f51b365bcb423b685899
toughCircle <toughCircle@gmail.com>
2024-09-09 15:55:01 +0900

    feat: 토큰 재발급 메서드 추가 (#6)

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/service/AuthenticationService.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/service/AuthenticationService.java
@@ -2,12 +2,17 @@ package Entry_BE_Assignment.auth_server.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import Entry_BE_Assignment.auth_server.entity.RefreshToken;
+import Entry_BE_Assignment.auth_server.entity.User;
+import Entry_BE_Assignment.auth_server.enums.StatusCode;
+import Entry_BE_Assignment.auth_server.exception.customException.BusinessException;
 import Entry_BE_Assignment.auth_server.jwt.JwtManager;
 import Entry_BE_Assignment.auth_server.repository.RefreshTokenRepository;
+import Entry_BE_Assignment.auth_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -15,15 +20,28 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class AuthenticationService {
 
+	private UserRepository userRepository;
 	private final RefreshTokenRepository refreshTokenRepository;
 	private final JwtManager jwtManager;
 
 	@Transactional
-	private void saveRefreshToken(String username, String refreshToken) {
+	public void saveRefreshToken(String username, String refreshToken) {
 		LocalDateTime expireDate = LocalDateTime.now().plusDays(7);
 		RefreshToken token = new RefreshToken(username, refreshToken, expireDate);  // 7일간 유효
 
 		refreshTokenRepository.deleteByUsername(username);  // 기존 토큰 삭제
+	}
+
+	public String refreshAccessToken(String refreshToken) {
+		if (jwtManager.isValidToken(refreshToken)) {
+			Long userId = jwtManager.getUserId(refreshToken);
+			User user = userRepository.findById(userId)
+				.orElseThrow(() -> new BusinessException(StatusCode.USER_NOT_FOUND));
+
+			return jwtManager.generateToken((Authentication)user);
+		} else {
+			throw new BusinessException(StatusCode.INVALID_REFRESH_TOKEN);
+		}
 	}
 
 }
