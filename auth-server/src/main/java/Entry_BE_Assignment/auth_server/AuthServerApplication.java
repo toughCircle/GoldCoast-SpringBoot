2116d5ef1a548a947c154d87a93db16466f40a38
toughCircle <toughCircle@gmail.com>
2024-09-11 08:20:02 +0900

    feat: Spring Boot와 gRPC 통합 (#9)

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/AuthServerApplication.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/AuthServerApplication.java
@@ -1,8 +1,19 @@
 package Entry_BE_Assignment.auth_server;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+import Entry_BE_Assignment.auth_server.jwt.JwtManager;
+import Entry_BE_Assignment.auth_server.repository.UserRepository;
+import Entry_BE_Assignment.auth_server.service.AuthServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +23,36 @@ public class AuthServerApplication {
 		SpringApplication.run(AuthServerApplication.class, args);
 	}
 
+}
+
+@Component
+class GrpcServerRunner {
+
+	@Value("${grpc.server.port}")
+	private int port;
+
+	private final UserRepository userRepository;
+	private final JwtManager jwtManager;
+
+	public GrpcServerRunner(UserRepository userRepository, JwtManager jwtManager) {
+		this.userRepository = userRepository;
+		this.jwtManager = jwtManager;
+	}
+
+	@PostConstruct
+	public void startGrpcServer() {
+		new Thread(() -> {
+			try {
+				Server server = ServerBuilder.forPort(port)
+					.addService(new AuthServiceImpl(userRepository, jwtManager))
+					.build()
+					.start();
+
+				System.out.println("Auth gRPC Server started on port " + port);
+				server.awaitTermination();
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
 }
