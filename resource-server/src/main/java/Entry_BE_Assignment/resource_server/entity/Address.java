4f805e31cff006cbdf45a4399713f1d8b63a404a
toughCircle <toughCircle@gmail.com>
2024-09-11 13:12:57 +0900

    feat: Entity 생성 (#10)

---
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/entity/Address.java
@@ -0,0 +1,47 @@
+package Entry_BE_Assignment.resource_server.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false) // 사용자의 기본 주소 업데이트를 위한 해시 비교
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "addresses")
+public class Address extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "addressId")
+	private Long id;
+
+	@EqualsAndHashCode.Include
+	private String zipCode;
+
+	@EqualsAndHashCode.Include
+	private String streetAddress;
+
+	@EqualsAndHashCode.Include
+	private String addressDetail;
+
+	public static Address createAddress(String zipCode, String streetAddress, String addressDetail) {
+		return Address.builder()
+			.zipCode(zipCode)
+			.streetAddress(streetAddress)
+			.addressDetail(addressDetail)
+			.build();
+	}
+
+}
---
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/entity/Order.java
@@ -0,0 +1,67 @@
+package Entry_BE_Assignment.resource_server.entity;
+
+import java.math.BigDecimal;
+
+import Entry_BE_Assignment.resource_server.enums.ItemType;
+import Entry_BE_Assignment.resource_server.enums.OrderStatus;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	@Enumerated(EnumType.STRING)
+	private ItemType itemType;
+
+	private BigDecimal quantity;
+
+	private BigDecimal price;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
+	private Address shippingAddress;
+
+	public static Order createOrder(User user, ItemType itemType, BigDecimal quantity, BigDecimal price,
+		Address shippingAddress) {
+		return Order.builder()
+			.user(user)
+			.status(OrderStatus.ORDERED)  // 기본 주문 상태
+			.itemType(itemType)
+			.quantity(quantity)
+			.price(price)
+			.shippingAddress(shippingAddress)
+			.build();
+	}
+
+}
