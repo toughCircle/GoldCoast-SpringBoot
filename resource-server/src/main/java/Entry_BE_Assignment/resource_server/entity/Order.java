7b398054b491e0e463b733c7fbfa6c7c470e22b2
toughCircle <toughCircle@gmail.com>
2024-09-11 19:57:03 +0900

    chore: 사용자 아이디, 주문번호 필드 생성 (#14)

--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/entity/Order.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/entity/Order.java
@@ -3,15 +3,14 @@ package Entry_BE_Assignment.resource_server.entity;
 import Entry_BE_Assignment.resource_server.enums.ItemType;
 import Entry_BE_Assignment.resource_server.enums.OrderStatus;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -32,34 +31,41 @@ public class Order extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+	@Column(nullable = false, unique = true)
+	private String orderNumber;  // Human Readable 형식의 주문 번호
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
+
+	@Column(nullable = false)
+	private Long userId;  // 인증 서버에서 받아온 사용자 ID
 
 	@Enumerated(EnumType.STRING)
 	private ItemType itemType;
 
 	private double quantity;
 
-	private double price;
+	private int price;
 
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "address_id", referencedColumnName = "id")
 	private Address shippingAddress;
 
-	public static Order createOrder(User user, ItemType itemType, double quantity, double price,
-		Address shippingAddress) {
+	public static Order createOrder(ItemType itemType, double quantity, int price,
+		Address shippingAddress, Long userId, String orderNumber) {
 		return Order.builder()
-			.user(user)
-			.status(OrderStatus.ORDERED)  // 기본 주문 상태
+			.status(OrderStatus.ORDER_PLACED)  // 기본 주문 상태
 			.itemType(itemType)
 			.quantity(quantity)
 			.price(price)
 			.shippingAddress(shippingAddress)
+			.userId(userId)
+			.orderNumber(orderNumber)
 			.build();
+	}
+
+	public void updateOrderStatus(OrderStatus orderStatus) {
+		this.status = orderStatus;  // 기존 주문의 상태를 업데이트
 	}
 
 }
