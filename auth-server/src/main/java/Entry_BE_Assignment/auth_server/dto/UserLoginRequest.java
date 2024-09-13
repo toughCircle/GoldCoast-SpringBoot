5432682f6ba1732ecfbb347a72002e54273b7f93
toughCircle <toughCircle@gmail.com>
2024-09-13 22:17:30 +0900

    chore: DTO 검증 조건, Swagger 설명 추가 (#19, #21)

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/dto/UserLoginRequest.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/dto/UserLoginRequest.java
@@ -1,15 +1,20 @@
 package Entry_BE_Assignment.auth_server.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class UserLoginRequest {
 
-	@NotBlank(message = "사용자 이름은 필수입니다.")
-	private String username;
+	@Schema(description = "사용자 이메일")
+	@Email(message = "유효한 이메일 형식이 아닙니다.")
+	@NotEmpty(message = "이메일은 필수 입력 값입니다.")
+	private String email;
 
-	@NotBlank(message = "비밀번호는 필수입니다.")
+	@Schema(description = "비밀번호")
+	@NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
 	private String password;
 
 }
--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/dto/UserRegisterRequest.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/dto/UserRegisterRequest.java
@@ -1,21 +1,31 @@
 package Entry_BE_Assignment.auth_server.dto;
 
-import Entry_BE_Assignment.auth_server.enums.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserRegisterRequest {
 
+	@Schema(description = "사용자 이름")
+	@NotEmpty(message = "사용자 이름은 필수 입력 값입니다.")
 	private String username;
-	@Email(message = "유효한 이메일 형식이어야 합니다.")
+
+	@Schema(description = "사용자 이메일")
+	@Email(message = "유효한 이메일 형식이 아닙니다.")
+	@NotEmpty(message = "이메일은 필수 입력 값입니다.")
 	private String email;
+
+	@Schema(description = "비밀번호")
+	@NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하이어야 합니다.")
 	@Pattern(
-		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+\\[\\]{};:'\",.<>?/|`~])[A-Za-z\\d!@#$%^&*()\\-_=+\\[\\]{};:'\",.<>?/|`~]{5,}$",
-		message = "비밀번호는 대문자 1개, 소문자 1개, 숫자 1개, 특수문자 2개 이상을 포함하고, 최소 5자리 이상이어야 합니다."
+		regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
+		message = "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
 	)
 	private String password;
-	private Role role;
 
 }
