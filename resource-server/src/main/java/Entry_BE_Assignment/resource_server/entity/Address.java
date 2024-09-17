7c8c58080c3f8c61537d199516e1744dc12e5c05
toughCircle <toughCircle@gmail.com>
2024-09-17 14:30:20 +0900

    chore: 불필요한 설정 제거 (#21)

--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/entity/Address.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/entity/Address.java
@@ -9,13 +9,11 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false) // 사용자의 기본 주소 업데이트를 위한 해시 비교
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
@@ -26,16 +24,12 @@ public class Address extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@EqualsAndHashCode.Include
-	@Column(nullable = false, length = 10)  // 우편번호는 최대 10자
+	@Column(nullable = false, length = 5)  // 우편번호는 최대 5자
 	private String zipCode;
 
-	@EqualsAndHashCode.Include
-	@Column(nullable = false, length = 255)  // 기본 주소는 최대 255자
+	@Column(nullable = false)
 	private String streetAddress;
 
-	@EqualsAndHashCode.Include
-	@Column(length = 255)  // 상세 주소는 선택 사항
 	private String addressDetail;
 
 	public static Address createAddress(String zipCode, String streetAddress, String addressDetail) {
