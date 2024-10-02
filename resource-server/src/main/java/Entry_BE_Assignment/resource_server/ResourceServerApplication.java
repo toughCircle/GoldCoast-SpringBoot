500a3312e00f9b56eb61e024d08ddad2965304ed
toughCircle <toughCircle@gmail.com>
2024-10-02 19:37:26 +0900

    feat: RestTemplate 빈 추가 (#21)

--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/ResourceServerApplication.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/ResourceServerApplication.java
@@ -2,7 +2,9 @@ package Entry_BE_Assignment.resource_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +12,11 @@ public class ResourceServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
