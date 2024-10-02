6d4623eba9429942d7bc960d05ddb5f9812f9804
toughCircle <toughCircle@gmail.com>
2024-10-02 19:43:14 +0900

    feat: 아이템 등록 로직 수정 (#21)

--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/service/ItemService.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/service/ItemService.java
@@ -1,15 +1,18 @@
 package Entry_BE_Assignment.resource_server.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import Entry_BE_Assignment.resource_server.dto.ItemRequest;
+import Entry_BE_Assignment.resource_server.entity.GoldPrice;
 import Entry_BE_Assignment.resource_server.entity.Item;
 import Entry_BE_Assignment.resource_server.enums.StatusCode;
 import Entry_BE_Assignment.resource_server.exception.customException.BusinessException;
 import Entry_BE_Assignment.resource_server.grpc.UserResponse;
+import Entry_BE_Assignment.resource_server.repository.GoldPriceRepository;
 import Entry_BE_Assignment.resource_server.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +22,8 @@ import lombok.RequiredArgsConstructor;
 public class ItemService {
 
 	private final ItemRepository itemRepository;
+	private final GoldPriceService goldPriceService;
+	private final GoldPriceRepository goldPriceRepository;
 
 	// 아이템 생성 (판매자만 가능)
 	@Transactional
@@ -27,7 +32,26 @@ public class ItemService {
 			throw new BusinessException(StatusCode.FORBIDDEN);
 		}
 
-		Item item = Item.createItem(itemRequest.getItemType(), itemRequest.getPrice(), userResponse.getUserId());
+		// 금 가격 조회
+		Optional<GoldPrice> goldPriceOptional = goldPriceRepository.findByGoldType(itemRequest.getItemType());
+
+		int pricePerGram;
+
+		if (goldPriceOptional.isPresent()) {
+			// 금 가격이 존재하면 해당 가격 사용
+			pricePerGram = goldPriceOptional.get().getPrice();
+		} else {
+			// 저장된 금 가격이 없을 경우 API 호출하여 조회 및 저장
+			String goldPriceResponse = goldPriceService.getGoldPriceInKRW();  // API 호출
+			pricePerGram = goldPriceService.extractPriceFromResponse(goldPriceResponse,
+				itemRequest.getItemType().name());
+
+			// 해당 타입의 금 시세 저장
+			goldPriceService.saveGoldPrice(itemRequest.getItemType(), pricePerGram);
+		}
+
+		Item item = Item.createItem(itemRequest.getItemType(), pricePerGram, itemRequest.getQuantity(),
+			userResponse.getUserId());
 
 		itemRepository.save(item);
 	}
@@ -52,7 +76,7 @@ public class ItemService {
 			throw new BusinessException(StatusCode.FORBIDDEN);
 		}
 
-		item.updateItem(itemRequest.getItemType(), itemRequest.getPrice());
+		item.updateItem(itemRequest.getItemType(), itemRequest.getQuantity());
 	}
 
 	// 아이템 삭제 (판매자만 가능)
