949411df330280b8b4832f6bc2845596e708427a
toughCircle <toughCircle@gmail.com>
2024-10-04 11:32:18 +0900

    feat: Swagger 의존성, 설정 수정 (#19)

--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/config/SwaggerConfig.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package Entry_BE_Assignment.resource_server.config;
 
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,7 +19,12 @@ public class SwaggerConfig {
 			.info(new Info()
 				.title("Auth-Resource-Project API")
 				.version("1.0")
-				.description("Auth-Resource 프로젝트의 자원서버 API 문서입니다."));
+				.description("Auth-Resource 프로젝트의 자원서버 API 문서입니다."))
+			.components(new Components()
+				.addSecuritySchemes("bearer-key",
+					new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+			.addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+
 	}
 
 	@Bean
