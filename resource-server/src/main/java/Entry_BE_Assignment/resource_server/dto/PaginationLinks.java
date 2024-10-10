a2bd063bd7469a89fd3189275f075f482710be67
toughCircle <toughCircle@gmail.com>
2024-10-10 23:17:58 +0900

    feat: 페이징 class 생성 (#20)

---
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/dto/PaginationLinks.java
@@ -0,0 +1,10 @@
+package Entry_BE_Assignment.resource_server.dto;
+
+public class PaginationLinks {
+
+	private String previous;
+	private String next;
+	private int totalPages;
+	private int currentPage;
+
+}
