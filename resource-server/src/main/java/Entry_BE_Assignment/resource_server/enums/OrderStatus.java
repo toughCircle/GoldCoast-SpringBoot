206e61ba66d86c221ce6df7062359379569e3f03
toughCircle <toughCircle@gmail.com>
2024-10-10 23:22:31 +0900

    feat: 주문 상태 추가 (#21)

--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/enums/OrderStatus.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/enums/OrderStatus.java
@@ -2,8 +2,12 @@ package Entry_BE_Assignment.resource_server.enums;
 
 public enum OrderStatus {
 	ORDER_PLACED,  // 주문 완료
+	ORDER_CANCELLED, // 주문 취소
+	REFUND_REQUESTED, // 환불 요청 (구매)
+	REFUND_COMPLETED, // 환불 완료 (판매)
+	RETURN_REQUESTED, // 반품 요청 (구매)
+	RETURN_COMPLETED, // 반품 완료 (판매)
 	PAYMENT_RECEIVED,  // 입금 완료 (구매)
 	SHIPPED,  // 발송 완료 (구매)
-	PAYMENT_SENT,  // 송금 완료 (판매)
 	RECEIVED  // 수령 완료 (판매)
 }
