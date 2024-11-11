6eea4a0fdc2412ab87724093af62feae7dab79da
toughCircle <toughCircle@gmail.com>
2024-11-11 21:56:26 +0900

    feat: RestTemplate 빈 등록 추가

---
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/config/AppConfig.java
@@ -0,0 +1,15 @@
+package Entry_BE_Assignment.auth_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+}
