bd407ae3cbc34c9f109aa981b0acdd08479f2596
toughCircle <toughCircle@gmail.com>
2024-11-13 13:24:18 +0900

    feat: RefreshToken 테이블 제거

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/service/AuthenticationService.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/service/AuthenticationService.java
@@ -1,7 +1,5 @@
 package Entry_BE_Assignment.auth_server.service;
 
-import java.time.LocalDateTime;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,12 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 import Entry_BE_Assignment.auth_server.dto.Tokens;
 import Entry_BE_Assignment.auth_server.dto.UserLoginRequest;
 import Entry_BE_Assignment.auth_server.dto.UserRegisterRequest;
-import Entry_BE_Assignment.auth_server.entity.RefreshToken;
 import Entry_BE_Assignment.auth_server.entity.User;
 import Entry_BE_Assignment.auth_server.enums.StatusCode;
 import Entry_BE_Assignment.auth_server.exception.customException.BusinessException;
 import Entry_BE_Assignment.auth_server.jwt.JwtManager;
-import Entry_BE_Assignment.auth_server.repository.RefreshTokenRepository;
 import Entry_BE_Assignment.auth_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -29,21 +25,9 @@ import lombok.RequiredArgsConstructor;
 public class AuthenticationService {
 
 	private final UserRepository userRepository;
-	private final RefreshTokenRepository refreshTokenRepository;
 	private final AuthenticationManager authenticationManager;
 	private final JwtManager jwtManager;
 	private final PasswordEncoder passwordEncoder;
-
-	@Transactional
-	public void saveRefreshToken(String username, String refreshToken) {
-		LocalDateTime expireDate = LocalDateTime.now().plusDays(7);
-
-		refreshTokenRepository.deleteByUsername(username);// 기존 토큰 삭제
-
-		RefreshToken token = new RefreshToken(username, refreshToken, expireDate);  // 7일간 유효
-
-		refreshTokenRepository.save(token);
-	}
 
 	public String refreshAccessToken(String refreshToken, String currentIpAddress, String currentUserAgent) {
 		if (jwtManager.isValidateRefreshToken(refreshToken, currentIpAddress, currentUserAgent)) {
@@ -93,8 +77,6 @@ public class AuthenticationService {
 
 			// JWT Refresh Token 생성
 			String refreshToken = jwtManager.generateRefreshToken(authentication, ipAddress, userAgent);
-
-			saveRefreshToken(userDetails.getUsername(), refreshToken);
 
 			return new Tokens(accessToken, refreshToken);
 
