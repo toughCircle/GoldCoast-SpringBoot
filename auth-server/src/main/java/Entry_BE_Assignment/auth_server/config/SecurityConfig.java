47c74ada4aabd4bd14cb85bc087350b198c63b5d
toughCircle <toughCircle@gmail.com>
2024-11-25 13:40:59 +0900

    feat: 스웨거 세팅 수정

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/config/SecurityConfig.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(authorizeRequests -> authorizeRequests
 				.requestMatchers(
 					"/swagger-ui/**",
+					"/swagger-ui/index.html",
 					"/v3/api-docs/**",
 					"/swagger-ui.html",
 					"/api-docs/**",
