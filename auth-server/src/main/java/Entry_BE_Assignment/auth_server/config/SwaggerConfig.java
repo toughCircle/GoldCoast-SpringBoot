8985338770f7d72f3d15f447f13eef8d941c5b72
toughCircle <toughCircle@gmail.com>
2024-11-27 14:44:22 +0900

    docs: 재설정

--- auth-server/src/main/java/Entry_BE_Assignment/auth_server/config/SwaggerConfig.java
+++ auth-server/src/main/java/Entry_BE_Assignment/auth_server/config/SwaggerConfig.java
@@ -1,30 +1,28 @@
 package Entry_BE_Assignment.auth_server.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
-	public GroupedOpenApi authApi() {
-		return GroupedOpenApi.builder()
-			.group("Auth API") // 인증 서버 API 그룹 이름
-			.pathsToMatch("/api/auth/**")
-			.build();
-	}
-
-	@Bean
 	public OpenAPI authOpenAPI() {
 		return new OpenAPI()
 			.info(new Info()
-				.title("Auth Server API")
-				.description("Auth Server 관련 API 문서입니다.")
-				.version("1.0"));
+				.title("Auth Server API") // API 이름
+				.description("Auth Server 관련 API 문서입니다.") // 설명
+				.version("1.0")) // 버전
+			.servers(Collections.singletonList(
+				new Server().url("http://java.gold-coast.shop") // 인증 서버의 기본 URL 설정
+					.description("Production Auth Server")
+			)); // 모든 API 호출에 보안 스키마 적용
 	}
 
 }
--- resource-server/src/main/java/Entry_BE_Assignment/resource_server/config/SwaggerConfig.java
+++ resource-server/src/main/java/Entry_BE_Assignment/resource_server/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package Entry_BE_Assignment.resource_server.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,17 +10,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
-
-	@Bean
-	public GroupedOpenApi resourceApi() {
-		return GroupedOpenApi.builder()
-			.group("Resource API") // 자원 서버 API 그룹 이름
-			.pathsToMatch("/api/**")
-			.build();
-	}
 
 	@Bean
 	public OpenAPI resourceOpenAPI() {
@@ -28,6 +22,10 @@ public class SwaggerConfig {
 				.title("Resource Server API")
 				.description("Resource Server 관련 API 문서입니다.")
 				.version("1.0"))
+			.servers(Collections.singletonList(
+				new Server().url("http://java.gold-coast.shop")
+					.description("Production Server")
+			))
 			.components(new Components()
 				.addSecuritySchemes("bearer-key",
 					new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
